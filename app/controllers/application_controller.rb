1b88d9a31581bea159cea8fbb0629b05253af659
Rabindranath Prithvi <rabindranath.prithvi@gmail.com>
2015-02-03 20:40:40 +0530

    Signup for student

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :persist_login_param
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
+
+  protected
+
+  def persist_login_param
+    session[:is_student] = params[:student]
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :is_student
+  end
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    courses_path
+  end
+
 end
---
+++ db/migrate/20150203134718_add_column_is_student_to_user.rb
@@ -0,0 +1,5 @@
+class AddColumnIsStudentToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :is_student, :boolean, default: false
+  end
+end
