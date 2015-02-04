40368eb859cc53d95caae33cc119086edb49e9e8
Rabindranath Prithvi <rabindranath.prithvi@gmail.com>
2015-02-04 21:47:17 +0530

    Professor can edit syllabus

--- app/controllers/syllabuses_controller.rb
+++ app/controllers/syllabuses_controller.rb
@@ -6,12 +6,26 @@ class SyllabusesController < ApplicationController
 	end
 
 	def show
-		@course = Course.find(params[:course_id])
-		@syllabuses = @course.syllabuses
+		@syllabus = Syllabus.find(params[:id])
 	end
 
 	def edit
 		@syllabus = Syllabus.find(params[:id])
+	end
+
+	def update
+		@syllabus = Syllabus.find(params[:id])
+		if @syllabus.update(syllabus_params)
+			redirect_to course_syllabus_path(@syllabus.course,@syllabus)
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+	def syllabus_params
+		params.require(:syllabus).permit(:code, :semester,:name)
 	end
 
 end
