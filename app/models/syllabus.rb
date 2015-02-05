bbc6b281cbe43bd2f39a6902d91b4a354d1b8543
Rabindranath Prithvi <rabindranath.prithvi@gmail.com>
2015-02-05 22:30:01 +0530

    Destroy chapters if syllabus is destroyed

--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -1,7 +1,7 @@
 class Syllabus < ActiveRecord::Base
 	belongs_to :course
-	has_many :chapters
-	
+	has_many :chapters, dependent: :destroy
+
 	validates :code , presence: true,  uniqueness: true
 	validates :name , presence: true
 	validates :semester , presence: true, numericality: { only_integer: true }	
